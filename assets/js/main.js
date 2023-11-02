505d8c9733bad83a9514d9cc2d67e084852e16cf
Judy Odiakose <ojudy007@gmail.com>
2023-11-02 01:17:21 +0100

    Finished the rest of the project and added animations because the other parts were short

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,7 +81,57 @@ function activeFeatured() {
 featuredLinks.forEach((link) => link.addEventListener("click", activeFeatured));
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollBtn = document.getElementById("scroll-up");
+const scrollAction = () => {
+  window.scrollY > 300
+    ? scrollBtn.classList.add("show-scroll")
+    : scrollBtn.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollAction);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__item a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+sr.reveal(
+  `.home__title, .popular__container, .features__banner, .featured__filters`
+);
+sr.reveal(`.home__subtitle`, { delay: 500 });
+sr.reveal(`.home__elec`, { delay: 600 });
+sr.reveal(`.home__banner`, { delay: 800 });
+sr.reveal(`.home__car-data`, { delay: 900, interval: 100, origin: "bottom" });
+sr.reveal(`.home__button`, { delay: 1000, origin: "bottom" });
+sr.reveal(`.about__group, .offer__data`, { origin: "left" });
+sr.reveal(`.about__data, .offer__banner`, { origin: "right" });
+sr.reveal(`.features__map`, { delay: 600, origin: "bottom" });
+sr.reveal(`.features__card`, { interval: 300 });
+sr.reveal(`.featured__card, .logo__content, .footer__content`, {
+  interval: 100,
+});
